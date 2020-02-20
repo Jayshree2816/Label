ffcc937ebfb8ede514236823be8d22892465c563
Jayshree Sonawane <32630349+Jayshree2816@users.noreply.github.com>
2020-02-20 16:20:03 +0530

    Edit label functionality added

--- script.js
+++ script.js
@@ -4,10 +4,11 @@ $(function(){
     $("#label-list").hide()
     $(".colorAlert").hide()
     $(".nameAlert").hide()
-
+    $("#label-form-1").hide()
     // Display label form
     $(".new-label").on("click", function(){
-        $(".label").toggle()
+        $(".label").show()
+        $(".label-preview").append($("#label-form"))
     })
 
     $("#label-name").keyup(function(){
@@ -25,8 +26,12 @@ $(function(){
     })
 
     // Cancel the form
-    $("#cancel").on("click", function(){
-        $(".label").hide() 
+    $("#cancel, #cancel-1 ").on("click", function(){
+        $(".label").hide()
+        $("input").val('')
+        $("#label-preview").text("Tag Preview")
+        $("#label-form-1").hide()
+
     })
 
     //Create new Label
@@ -44,9 +49,10 @@ $(function(){
                 $(".nameAlert").hide()
                 $(".colorAlert").hide()
                 $("#label-list").show()
-                $("#label-list").append("<div class='label-value'><li>" + label + "</li><button class='delete'>" + "Delete" + "</button>")
+                $("#label-list").append("<div class='label-value'><li>" + label + "</li><button class='delete'>" + "Delete" + "</button><button class='edit'>" + "Edit" + "</button>")
                 $("li:last()").css("background-color" , colorValue)
                 $("input").val('')
+                $("#label-preview").text("Tag Preview")
             }
         }
 
@@ -56,11 +62,26 @@ $(function(){
             $('.label-list:not(:has(li))').hide()
         })
 
-        // var emptyList = $(".label-list li")
-        // if(emptyList.length === 0) {
-        //     $(".label-list").hide()
-        // }
+        $(".edit").on("click", function(){
+            $(this).hide()
+            $(".label-value").append($("#label-form-1").show())
+            $("#label-name-1").val($("li").text())
+            $("#color-val-1").val(colorValue) 
+        })
 
+        $("#reset-1").on("click", function(){
+            var color = '#' + Math.floor(Math.random() * 16777215).toString(16)
+            $("#color-val-1").val(color)
+            $(this).css("background-color", color)
+            $("li").css("background-color", color)  
+        })
+
+        $("#create-label-1").on("click", function(){
+            $("li").text($("#label-name-1").val())
+            $("#label-form-1").hide()
+            $("input").val('')
+            $(".edit").show()
+        })
        e.preventDefault()
     })  
 })
